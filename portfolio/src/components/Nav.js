268adff78b29b7902189a04e0e5bd08528c49e0e
Sandy Caba <joriel@sandys-air.mynetworksettings.com>
2022-10-29 18:21:52 -0400

    added imgs for socials, no href yet

--- portfolio/src/components/Nav.js
+++ portfolio/src/components/Nav.js
@@ -6,6 +6,14 @@ function Nav() {
       <h2>About Me</h2>
       <h2>Projects</h2>
       <h2>Contact</h2>
+      <img
+        src="https://github.githubassets.com/images/modules/logos_page/Octocat.png"
+        alt="github logo"
+      />
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/LinkedIn_logo_initials.png/800px-LinkedIn_logo_initials.png"
+        alt="linkedin page"
+      />
     </div>
   );
 }

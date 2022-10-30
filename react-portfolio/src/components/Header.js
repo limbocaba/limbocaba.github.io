cea3bec6077b634332b1d73e096f06907e8f7277
Sandy Caba <joriel@sandys-air.mynetworksettings.com>
2022-10-29 20:11:57 -0400

    revamping project again, if code break go to last commit

---
+++ react-portfolio/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import "./Header.css"
+
+function Header() {
+  return (
+    <div className='header'>
+      <div className='name'>
+        <h2>Sandy Caba</h2>
+        <h3>Full Stack Engineer</h3>
+      </div>
+    </div>
+  )
+}
+
+export default Header

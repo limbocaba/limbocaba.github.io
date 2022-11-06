dd4c6f5cf52af450caef9ab67228a7e2600044df
Sandy Caba <joriel@sandys-air.mynetworksettings.com>
2022-11-06 15:07:04 -0500

    blue background, deleted files

--- react-portfolio/src/App.js
+++ react-portfolio/src/App.js
@@ -1,12 +1,9 @@
 import './App.css';
-import Header from './components/Header';
-import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      <Nav />
-      <Header />
+    
     </div>
   );
 }

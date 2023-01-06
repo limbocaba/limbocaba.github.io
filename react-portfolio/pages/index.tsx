d9a7b60199573d6b62b2dea739d3c9a154b77210
Sandy Caba <joriel@sandys-air.mynetworksettings.com>
2023-01-06 16:35:43 -0500

    added demo and github links to the projects and fixed responsiveness issue

--- react-portfolio/pages/index.tsx
+++ react-portfolio/pages/index.tsx
@@ -51,11 +51,27 @@ export default function Home() {
   }
 
   const handleClickBred = () => {
-    window.open("https://breddit.netlify.app/","_blank")
+    window.open("https://github.com/SEI-Buffleheads/reddit-clone","_blank")
   }
 
   const handleClickEmail = () => {
     window.open('mailto:cabassndy@gmail.com')
+  }
+
+  const handleClickHuluDemo = () => {
+    window.open("https://sandys-clone.surge.sh/","_blank")
+  }
+
+  const handleClickPokeDemo = () => {
+    window.open("https://sandys-pokedex.netlify.app/","_blank")
+  }
+
+  const handleClickPetDemo = () => {
+    window.open("https://pet-lyfe.netlify.app/","_blank")
+  }
+
+  const handleClickBredDemo = () => {
+    window.open("https://breddit.netlify.app/","_blank")
   }
 
   return (
@@ -193,58 +209,92 @@ export default function Home() {
           <About />
           <div>
             <div className="flex justify-center">
-              <h3 className="text-3xl py-1 justify-center underline font-semibold dark:text-purple-400">
+              <h3 className="text-2xl py-1 justify-center uppercase dark:text-purple-400 tracking-wider text-[#5651e5]">
                 Projects
               </h3>
             </div>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-white"></p>
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap p-20">
-            <div className="basis-1/3 flex-1 w-[494px] h-[295px] m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 hover:shadow-blue-100 flex-col">
+            <div className="basis-1/3 flex-1 w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 hover:shadow-blue-100 flex-col">
               <Image
                 src={hulu}
-                height={200}
-                width={350}
+                height={250}
+                width={500}
                 className="rounded-lg object-cover cursor-default"
                 alt=""
               />
               <div className=" flex">
-              <h3 className=" rounded-lg shadow-lg bg-black text-white my-4 cursor-pointer py-3 px-3 mx-3 hover:bg-gray-400">Demo</h3>
+                <h3
+                  onClick={handleClickHuluDemo}
+                  className=" rounded-lg shadow-lg bg-black text-white my-4 cursor-pointer py-3 px-3 mx-3 hover:bg-gray-400 tracking-wider">Demo</h3>
                 <h3
                   onClick={handleClickHulu}
-                  className=" rounded-lg shadow-lg bg-black text-white my-4 cursor-pointer py-3 px-3 mx-3 hover:bg-gray-400">
+                  className=" rounded-lg shadow-lg bg-black text-white my-4 cursor-pointer py-3 px-3 mx-3 hover:bg-gray-400 tracking-wider">
                   GitHub
                 </h3>
                 </div>
             </div>
-            <div className="basis-1/3 flex-1">
+            <div className="basis-1/3 flex-1 w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 hover:shadow-blue-100 flex-col">
               <Image
                 src={pokedex}
-                className="rounded-lg object-cover cursor-pointer hover:brightness-50"
+                height={250}
+                width={500}
+                className="rounded-lg object-cover"
                 alt=""
-                onClick={handleClickPoke}
               />
+              <div className=" flex">
+                <h3
+                  onClick={handleClickPokeDemo}
+                  className=" rounded-lg shadow-lg bg-black text-white my-4 cursor-pointer py-3 px-3 mx-3 hover:bg-gray-400 tracking-wider">Demo</h3>
+                <h3
+                  onClick={handleClickPoke}
+                  className=" rounded-lg shadow-lg bg-black text-white my-4 cursor-pointer py-3 px-3 mx-3 hover:bg-gray-400 tracking-wider">
+                  GitHub
+                </h3>
+                </div>
             </div>
-            <div className="basis-1/3 flex-1">
+            <div className="basis-1/3 flex-1 w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 hover:shadow-blue-100 flex-col">
               <Image
                 src={petLyfe}
-                className="rounded-lg object-cover cursor-pointer transition hover:-translate-y-1 hover:scale-110 hover:brightness-50"
+                height={250}
+                width={500}
+                className="rounded-lg object-cover"
                 alt=""
-                onClick={handleClickPet}
               />
+              <div className=" flex">
+                <h3
+                  onClick={handleClickPetDemo}
+                  className=" rounded-lg shadow-lg bg-black text-white my-4 cursor-pointer py-3 px-3 mx-3 hover:bg-gray-400 tracking-wider">Demo</h3>
+                <h3
+                  onClick={handleClickPet}
+                  className=" rounded-lg shadow-lg bg-black text-white my-4 cursor-pointer py-3 px-3 mx-3 hover:bg-gray-400 tracking-wider">
+                  GitHub
+                </h3>
+                </div>
             </div>
-            <div className="basis-1/3 flex-1">
+            <div className="basis-1/3 flex-1 w-full h-auto m-auto shadow-xl shadow-gray-400 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300 hover:shadow-blue-100 flex-col">
                <Image
                 src={breddit}
+                height={250}
+                width={500}
                 alt=""
-                className="rounded-lg object-cover cursor-pointer transition hover:-translate-y-1 hover:scale-110 hover:brightness-50"
-                onClick={handleClickBred}
+                className="rounded-lg object-cover"
               /> 
+              <div className=" flex">
+                <h3
+                  onClick={handleClickBredDemo}
+                    className=" rounded-lg shadow-lg bg-black text-white my-4 cursor-pointer py-3 px-3 mx-3 hover:bg-gray-400 tracking-wider">Demo</h3>
+                <h3
+                  onClick={handleClickBred}
+                  className=" rounded-lg shadow-lg bg-black text-white my-4 cursor-pointer py-3 px-3 mx-3 hover:bg-gray-400 tracking-wider">
+                  GitHub
+                  </h3>
+                </div>
             </div>
           </div>
         </section>
       </main>
-      {/* <About /> */}
     </div>
   );
 }
